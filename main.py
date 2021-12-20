552b7f5bae655695e18d3fb830673a914a8a7552
Pavlos Kallis <pkallis@douleutaras.com>
2021-12-20 21:45:46 +0200

    Add example in the API

--- main.py
+++ main.py
@@ -40,6 +40,26 @@ class Inference(BaseModel):
     hours_per_week: int
     native_country: str
 
+    class Config:
+        schema_extra = {
+            "example": {
+                'age': 52,
+                'workclass': 'Self-emp-inc',
+                'fnlgt': 287927,
+                'education': 'HS-grad',
+                'education_num': 9,
+                'marital_status': 'Married-civ-spouse',
+                'occupation': 'Exec-managerial',
+                'relationship': 'Wife',
+                'race': 'White',
+                'sex': 'Female',
+                'capital_gain': 15024,
+                'capital_loss': 0,
+                'hours_per_week': 40,
+                'native_country': 'United-States',
+            }
+        }
+
 
 @app.post("/inference/")
 async def inference(data: Inference):
